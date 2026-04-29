0da419fe35e51f5c82278d4b21fa15850d7478ed
xinyi-729 <yoxi07291128@gmail.com>
2026-04-29 15:57:51 +0200

    probando render

--- songproject/song_models/admin.py
+++ songproject/song_models/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Song, SongUser
 
-# Register your models here.
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'artist', 'language', 'category', 'number_times_played')
+    list_filter = ('language', 'category')
+    search_fields = ('title', 'artist')
+    readonly_fields = ('created_at',)
+
+@admin.register(SongUser)
+class SongUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'song', 'played_at', 'correct_guesses', 'wrong_guesses')
+    list_filter = ('played_at',)
+    search_fields = ('user__username', 'song__title')
+    readonly_fields = ('played_at',)
--- songproject/songproject/settings.py
+++ songproject/songproject/settings.py
@@ -161,7 +161,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (uploads)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Buscar archivos en la carpeta que realmente contiene los archivos
+if os.path.exists(os.path.join(BASE_DIR, "Media")):
+    MEDIA_ROOT = os.path.join(BASE_DIR, "Media")
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
