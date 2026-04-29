e48d87c3264ad3b593197c46d1cd7fa84e473604
xinyi-729 <yoxi07291128@gmail.com>
2026-04-29 16:18:57 +0200

    probando render

--- songproject/songproject/settings.py
+++ songproject/songproject/settings.py
@@ -195,6 +195,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
+    "https://p4-songproject-frontend-06-2312-2026-1.onrender.com",
 ]
 
 # 
